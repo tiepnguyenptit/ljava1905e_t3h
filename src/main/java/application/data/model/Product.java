a6d4632b8acca29cf12743c79982d45ab6e06052
tiepnn <tiepnguyenptit@gmail.com>
2019-10-15 21:01:39 +0700

    add entity product

---
+++ src/main/java/application/data/model/Product.java
@@ -0,0 +1,78 @@
+package application.data.model;
+
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+@Entity(name = "dbo_product")
+public class Product {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "product_id")
+    @Id
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "short_desc")
+    private String shortDesc;
+
+    @Column(name = "main_image")
+    private String mainImage;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(name = "created_date")
+    private Date createdDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getShortDesc() {
+        return shortDesc;
+    }
+
+    public void setShortDesc(String shortDesc) {
+        this.shortDesc = shortDesc;
+    }
+
+    public String getMainImage() {
+        return mainImage;
+    }
+
+    public void setMainImage(String mainImage) {
+        this.mainImage = mainImage;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+}
