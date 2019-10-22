4a2ebd999de8f7e6a1ec38091825eccd66e9afe2
tiepnn <tiepnguyenptit@gmail.com>
2019-10-22 20:34:32 +0700

    fake product

--- src/main/java/application/controller/api/ProductApiController.java
+++ src/main/java/application/controller/api/ProductApiController.java
@@ -1,6 +1,7 @@
 package application.controller.api;
 
 
+import application.data.service.CategoryService;
 import application.data.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,10 +15,15 @@ public class ProductApiController {
     @Autowired
     private ProductService productService;
 
+    @Autowired
+    private CategoryService categoryService;
 
-    @GetMapping("")
-    public String test() {
-        return "Ljava1905e";
-    }
+    private String[] images = {
+            "https://salt.tikicdn.com/cache/550x550/ts/product/0a/fb/75/740106b009f436911a8ea4efdf7edadf.jpg",
+            "https://salt.tikicdn.com/cache/550x550/media/catalog/product/a/m/american-edition-5-student-book.jpg",
+            "https://salt.tikicdn.com/cache/w1200/ts/product/cc/6f/1a/bddcfae10b1ae4877dee0d85d11a325e.jpg",
+            "https://salt.tikicdn.com/cache/w1200/ts/product/00/47/df/b02b462394bc3c59e5876ec0d9cb6ae8.jpg",
+            "https://salt.tikicdn.com/cache/550x550/ts/product/dd/28/91/4a7bb0e7be810aade0c4ab45427508a4.jpg"
+    };
 
 }
