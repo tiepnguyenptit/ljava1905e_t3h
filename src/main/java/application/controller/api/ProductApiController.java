7bbf3a42925551c5cf5a3d7b9264f07b1ac0b58c
tiepnn <tiepnguyenptit@gmail.com>
2019-10-24 19:53:42 +0700

    product api detail

--- src/main/java/application/controller/api/ProductApiController.java
+++ src/main/java/application/controller/api/ProductApiController.java
@@ -6,6 +6,8 @@ import application.data.model.Product;
 import application.data.service.CategoryService;
 import application.data.service.ProductService;
 import application.model.api.BaseApiResult;
+import application.model.api.DataApiResult;
+import application.model.dto.ProductDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -76,4 +78,40 @@ public class ProductApiController {
         }
         return result;
     }
+
+
+
+    @GetMapping("/detail/{productId}")
+    public DataApiResult detailMaterial(@PathVariable int productId){
+        DataApiResult result= new DataApiResult();
+
+        try {
+            Product productEntity = productService.findOne(productId);
+            if(productEntity == null) {
+                result.setSuccess(false);
+                result.setMessage("Can't find this product");
+            } else {
+                ProductDTO dto = new ProductDTO();
+                dto.setId(productEntity.getId());
+                if(productEntity.getCategory() != null) {
+                    dto.setCategoryId(productEntity.getCategory().getId());
+                }
+                dto.setMainImage(productEntity.getMainImage());
+                dto.setName(productEntity.getName());
+                dto.setPrice(productEntity.getPrice());
+                dto.setShortDesc(productEntity.getShortDesc());
+                dto.setCreatedDate(new Date());
+                result.setSuccess(true);
+                result.setData(dto);
+                result.setMessage("Success");
+            }
+        } catch (Exception e) {
+            result.setSuccess(false);
+            result.setMessage(e.getMessage());
+        }
+
+        return result;
+    }
+
+
 }
